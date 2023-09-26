426a2c24af876bee0f063ca8c2dfa762fb41e42c
Asztrik <105195963+aszpik12@users.noreply.github.com>
2023-09-26 15:50:22 +0200

    controlerek

--- library/app/Http/Controllers/BookController.php
+++ library/app/Http/Controllers/BookController.php
@@ -2,9 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    //
+    public function index(){
+        return Book::all();
+    }
+
+    public function show($id){
+        return Book::find($id);
+    }
+
+    public function destroy($id){
+        Book::find($id)->delete;
+        return redirect('/book/list');
+    }
+
+    public function update(Request $request, $id){
+        $book = Book::find($id);
+        $book->author = $request->author;
+        $book->title = $request->title;
+        $book->pieces = $request->pieces;
+        $book->save();
+        return redirect('/book/list');
+    }
+
+    public function store(Request $request){
+        $book = new Book();
+        $book->author = $request->author;
+        $book->title = $request->title;
+        $book->pieces = $request->pieces;
+        $book->save();
+        return redirect('/book/list');
+    }
 }
